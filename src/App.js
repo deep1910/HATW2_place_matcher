9ab4316824919b9d72641bb9a38f7d84d9158148
Mohit <mohit.ashliya@gmail.com>
2023-02-25 09:26:09 +0530

    added project strucutre

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import PlacePage from './pages/PlacePage';
 
 function App() {
   return (
     <div className="App">
-      our project starts here
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/page' element={<PlacePage/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/PlacePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PlacePage.css'
+
+const PlacePage = () => {
+  return (
+    <div>PlacePage</div>
+  )
+}
+
+export default PlacePage
